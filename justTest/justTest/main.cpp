185904c66e896192dfefa727a2379bbb032dd9da
Yaning Wang <2667126427@qq.com>
2017-03-26 08:55:45 +0800

    just test.

---
+++ justTest/justTest/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>	
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello GitHub" << endl;
+
+	return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string test = "Hello GitHub!\n";
+
+	cout << test;
+
+
+
+	return 0;
+}
