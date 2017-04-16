9c6ec153a1d1c0daa7aab52ee1b20dccfdaed188
Yaning Wang <Yaning Wang>
2017-04-16 11:19:29 +0800

    Left questions.

--- 34SelectSort/main.c
+++ 34SelectSort/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <malloc.h>
+void Read(int* arr, int N){
+    for (int i = 0; i < N; ++i){
+        scanf("%d", &arr[i]);
+    }
+}
+
+void SeleSort(int*arr, int N){
+    int min = 0;
+    for (int i = 0; i < N; ++i){
+        min = i;
+        for (int j = i + 1; j < N; ++j){
+            if (arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        if (min != i){
+            int t = arr[i];
+            arr[i] = arr[min];
+            arr[min] = t;
+        }
+    }
+}
+
+void Print(int* arr, int N){
+    printf("%d", arr[0]);
+    for (int i = 1; i < N; ++i){
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
-
+    int N = 0;
+    int *arr = NULL;
+    while ((scanf("%d", &N), N) != 0){
+        arr = (int*)malloc(N * sizeof(int));
+        Read(arr, N);
+        SeleSort(arr, N);
+        Print(arr, N);
+        free(arr);
+        arr = NULL;
+    }
     return 0;
 }
--- 上机/03第三次上机/4.3GoldBaches/main.c
+++ 上机/03第三次上机/4.3GoldBaches/main.c
@@ -1,53 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 #define BEGIN 10
 #define END 20
-
-int IsPrime(int N)
-{
-    if (N == 1)
-    {
-        return 0;
-    }
-    for (int i = 2; i <= sqrt(N); ++i)
-    {
-        if (N % i == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
+int IsPrime(int N) {
+	for (int i = 3; i <= N * 0.5; ++i) {
+		if (N % i == 0) {
+			return 0;
+		}
+		++i;
+	}
+	return 1;
 }
-
-int Prime(int N)
-{
-    for (int i = 1; i <= N; ++i)
-    {
-        if (IsPrime(i) && IsPrime(N - i))
-        {
-            return i;
-        }
-    }
-    return -1;
+// 用来获取满足条件的N的第一个素数也是较小的
+int GetPrime(int N) {
+	// 考虑4的特殊性
+	if (N == 4) {
+		return 2;
+	}
+	for (int i = 3; i <= N * 0.5; ++i) {
+		if (IsPrime(i) && IsPrime(N - i)) {
+			return i;
+		}
+		++i;
+	}
+	return -1;
 }
-
-int main()
-{
-    int numA = 0, numB = 0;
-    int t = 0;
-    numA = BEGIN;
-    numB = END;
-    printf("GOLDBACH'S CONJECTURE:\n");
-    printf("Every even number n >= 4 is the sum of two primes.\n");
-    if (numA % 2 == 1)
-    {
-        ++numA;
-    }
-    for (int i = numA; i <= numB; ++i)
-    {
-        t = Prime(i);
-        printf("%d = %d + %d\n", i, t, i - t);
-        ++i;
-    }
-    return 0;
+int main() {
+	int numA = 0, numB = 0;
+	int t = 0;
+	numA = BEGIN;
+	numB = END;
+	printf("GOLDBACH'S CONJECTURE:\n");
+	printf("Every even number n >= 4 is the sum of two primes.\n");
+	// 判断初始值奇偶
+	if (numA % 2 == 1) {
+		++numA;
+	}
+	for (int i = numA; i <= numB; ++i) {
+		t = GetPrime(i);
+		printf("%d = %d + %d\n", i, t, i - t);
+		++i;
+	}
+	return 0;
 }
