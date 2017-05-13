ec31d289855066fbcc518ae3442f8c805593f9d1
ya-ning <wangyaning6166@gmail.com>
2017-05-13 20:10:29 +0800

    Fixed errors and update report

--- 001OnComputer/06/04.3Sort.c
+++ 001OnComputer/06/04.3Sort.c
@@ -10,8 +10,9 @@
 
 // 有参数减序
 int Desce(int a, int b){
-    return a <= b;
+    return a <= b; //至于为什么要等于，这是经过教训得出的
 }
+// 无参数升序
 int Ascen(int a, int b){
     return a >= b;
 }
@@ -28,17 +29,17 @@ void Print(int* arr, int N){
 
 void Sort(int *arr, const int start, const int end, int (*comp)(int a, int b)){
     if (start >= end)return;
-    int key = arr[start];
+    int key = arr[start]; // 选取中间数
     int s = start;
     int e = end;
     while(s < e){
-        while(comp(arr[e], key) && s < e)--e;
-        arr[s] = arr[e];
+        while(comp(arr[e], key) && s < e)--e; // 当key和arr[e]满足大小比较时继续往下走
+        arr[s] = arr[e]; // 不满足了交换
         while(comp(key,arr[s]) && s < e)++s;
         arr[e] = arr[s];
     }
-    arr[s] = key;
-    Sort(arr, start, s - 1, comp);
+    arr[s] = key; // 将key放在制定位置
+    Sort(arr, start, s - 1, comp); // 递归向下
     Sort(arr, s + 1, end, comp);
 }
 
@@ -52,5 +53,6 @@ int main(int argc, char*argv[]){
     // argc 为 1 没有-d，2有
     Sort(arr, 0, N - 1, (argc - 1) ? Desce : Ascen);
     Print(arr, N);
+    free(arr);
     return 0;
 }
