35e834db49ae3e6a87d62694792bb206eb015c37
ya-ning <wangyaning6166@gmail.com>
2017-06-23 10:46:00 +0800

    Change directory

--- 001OnComputer/05/testCode.c
+++
@@ -1,9 +0,0 @@
-﻿#include <stdio.h>
-
-int main(void)
-{
-	long long ago;
-	double kill;
-	
-	return 0;
-}
--- test/ff.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-   printf("%i\n", 0); 
-    return 0;
-}
