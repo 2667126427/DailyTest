80f4102d1a2b5c5b0edcb481e7d238d9c2199c97
ya-ning <wangyaning6166@gmail.com>
2017-07-25 22:56:52 +0800

    测试宏

---
+++ c/test/testDefine.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//#line 6 "string"
+
+int main(void) {
+	//char *str = "xyz";
+	//printf("%s\n", str + 4);
+	//int t = 0;/*
+	/*scanf("%d", &t);
+
+	printf("%c\n", "0123456789ABCDEF"[t % 16]);*/
+	printf("%s %s", __DATE__,  __TIME__);
+
+	return 0;
+}
