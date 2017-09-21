be9bdbcc6d9838ebe6c24df0011a2360103fe07c
hustr <wangyaning6166@gmail.com>
2017-09-21 19:44:04 +0800

    update

--- java/tcpip/Server.java
+++ java/tcpip/Server.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
+package SendMessage;
+
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static final int PORT = 12345;//监听的端口号
+    private static final int PORT = 12345;//监听的端口号
 
     public static void main(String[] args) {
         System.out.println("服务器启动...\n");
@@ -14,7 +13,7 @@ public class Server {
         server.init();
     }
 
-    public void init() {
+    private void init() {
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
             while (true) {
@@ -31,7 +30,7 @@ public class Server {
     private class HandlerThread implements Runnable {
         private Socket socket;
 
-        public HandlerThread(Socket client) {
+        HandlerThread(Socket client) {
             socket = client;
             new Thread(this).start();
         }
@@ -43,16 +42,6 @@ public class Server {
                 String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
                 // 处理客户端数据
                 System.out.println("客户端发过来的内容:" + clientInputStr);
-
-                // 向客户端回复信息
-//                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-//                System.out.print("请输入:\t");
-                // 发送键盘输入的一行
-//                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-//                out.writeUTF(s);
-
-//                out.close();
-//                input.close();
             } catch (Exception e) {
                 System.out.println("服务器 run 异常: " + e.getMessage());
             } finally {
