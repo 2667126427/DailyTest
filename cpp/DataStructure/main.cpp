32e0b5e4a0a403613a733259855bb7b029405a77
hustr <wangyaning6166@gmail.com>
2017-12-14 22:30:20 +0800

    solve conflict

--- cpp/DataStructure/main.cpp
+++ cpp/DataStructure/main.cpp
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 // 包含头文件
 #include "testLinkedList.h"
 
 int main() {
     // 测试函数
     testLinkedList();
-=======
-#include "testLinkedList.h"
-//#include "testLinearList.h"
-
-int main() {
-    testLinkedList();
-	//testLinearList();
->>>>>>> master
 
     return 0;
 }
